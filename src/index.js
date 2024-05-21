aa1f3fd14b27b713d7c443d1f60d18c298919bf2
ilia.m <ilmksmv@gmail.com>
2024-05-21 12:34:31 +0200

    step 3, predone2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import makeAbsolutePath from "../path.js";
+import fs from 'fs';
 
+const genDiff = (filepath1, filepath2) => {
+  const firstFilePath = makeAbsolutePath(filepath1);
+  const secondFilePath = makeAbsolutePath(filepath2);
 
-const genDiff = (filepath1, filepath2) => {};
+  const readFirstFile = fs.readFileSync(firstFilePath, 'utf-8');
+  const readSecondFile = fs.readFileSync(secondFilePath, 'utf-8');
+};
 
 export default genDiff;
