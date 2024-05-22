84f91d62b8ba3709db4f992d44f255f483a4cebf
ilia.m <ilmksmv@gmail.com>
2024-05-22 15:44:42 +0200

    edited

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import parse from './parse.js';
 const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFileType = (filepath) => path.extname(filepath).slice(1);
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
-const dataParse = (filepath) => parse(filepath);
 
 const genDiff = (filepath1, filepath2) => {};
 
