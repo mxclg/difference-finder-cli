bfcbce012e6d2e9f7b898bcc669bf1d469ed0a44
ilia.m <ilmksmv@gmail.com>
2024-05-30 15:41:50 +0200

    wrote func for json and yaml parse

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,15 @@
-const parse = (data) => JSON.parse(data);
+import yaml from 'js-yaml';
 
-export default parse;
+const parsingData = (data, extension) => {
+  let result;
+  if (extension === 'yaml' || extension === 'yml') {
+    result = yaml.load(data);
+  } else if (extension === 'json') {
+    result = JSON.parse(data);
+  } else {
+    throw new Error(`Unsupported format: ${extension}`);
+  }
+  return result;
+};
+
+export default parsingData;
