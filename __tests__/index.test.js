fdf43605e0de0c5f99255b92539e6192f0d349c5
ilia.m <ilmksmv@gmail.com>
2024-05-30 15:44:29 +0200

    wrote test for gen diff flat yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,9 +10,16 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-test('compare plain .json files', () => {
+test('compare flat json files', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
-  const result = readFile('plainJsonDiff.txt');
+  const result = readFile('stringDiffReport.txt');
+  expect(genDiff(filePath1, filePath2)).toEqual(result);
+});
+
+test('compare flat yaml files', () => {
+  const filePath1 = getFixturePath('file1.yml');
+  const filePath2 = getFixturePath('file2.yml');
+  const result = readFile('stringDiffReport.txt');
   expect(genDiff(filePath1, filePath2)).toEqual(result);
 });
