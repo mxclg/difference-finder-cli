7ea7bbc1bbfe219ad0b78a509805e1061d096c70
ilia.m <ilmksmv@gmail.com>
2024-06-07 16:52:44 +0200

    fix

--- src/parse.js
+++ src/parse.js
@@ -1,15 +1,13 @@
 import yaml from 'js-yaml';
 
 const parsingData = (data, extension) => {
-  let result;
   if (extension === 'yaml' || extension === 'yml') {
-    result = yaml.load(data);
-  } else if (extension === 'json') {
-    result = JSON.parse(data);
-  } else {
-    throw new Error(`Unsupported format: ${extension}`);
+    return yaml.load(data);
   }
-  return result;
+  if (extension === 'json') {
+    return JSON.parse(data);
+  }
+  throw new Error(`Unsupported format: ${extension}`);
 };
 
 export default parsingData;
